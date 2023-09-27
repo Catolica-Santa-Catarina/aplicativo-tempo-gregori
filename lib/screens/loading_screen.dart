0a2fbcbc498f66443c30ae86b83358c2cd47afd9
Rodrigo Gregori <rodrigo.gregori@catolicasc.org.br>
2023-09-27 20:23:00 -0300

    Obtendo localização

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({Key? key}) : super(key: key);
@@ -8,6 +9,36 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  Future<void> getLocation() async {
+    checkLocationPermission();
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    print(position);
+  }
+
+  Future<void> checkLocationPermission() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // serviço de localização desabilitado. Não será possível continuar
+      return Future.error('O serviço de localização está desabilitado.');
+    }
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Sem permissão para acessar a localização
+        return Future.error('Sem permissão para acesso à localização');
+      }
+    }
+    if (permission == LocationPermission.deniedForever) {
+      // permissões negadas para sempre
+      return Future.error(
+          'A permissão para acesso a localização foi negada para sempre. Não é possível pedir permissão.');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,6 +46,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
         child: ElevatedButton(
           onPressed: () {
             // obtém a localização atual
+            getLocation();
           },
           child: const Text('Obter Localização'),
         ),
