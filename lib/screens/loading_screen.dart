d5246c5b6509c2fe783fe79ede0dfac28ef74486
Rodrigo Gregori <rodrigo.gregori@catolicasc.org.br>
2023-09-27 21:32:28 -0300

    separar lógica de localização

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,7 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
+import 'package:tempo_template/services/location.dart';
 
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({Key? key}) : super(key: key);
@@ -10,33 +12,18 @@ class LoadingScreen extends StatefulWidget {
 
 class _LoadingScreenState extends State<LoadingScreen> {
   Future<void> getLocation() async {
-    checkLocationPermission();
-    Position position = await Geolocator.getCurrentPosition(
-        desiredAccuracy: LocationAccuracy.high);
-    print(position);
+    var location = Location();
+    await location.getCurrentLocation();
+
+    log(
+      'Latitude: ${location.latitude}, Longitude: ${location.longitude}',
+    );
   }
 
-  Future<void> checkLocationPermission() async {
-    bool serviceEnabled;
-    LocationPermission permission;
-    serviceEnabled = await Geolocator.isLocationServiceEnabled();
-    if (!serviceEnabled) {
-      // serviço de localização desabilitado. Não será possível continuar
-      return Future.error('O serviço de localização está desabilitado.');
-    }
-    permission = await Geolocator.checkPermission();
-    if (permission == LocationPermission.denied) {
-      permission = await Geolocator.requestPermission();
-      if (permission == LocationPermission.denied) {
-        // Sem permissão para acessar a localização
-        return Future.error('Sem permissão para acesso à localização');
-      }
-    }
-    if (permission == LocationPermission.deniedForever) {
-      // permissões negadas para sempre
-      return Future.error(
-          'A permissão para acesso a localização foi negada para sempre. Não é possível pedir permissão.');
-    }
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
   }
 
   @override
